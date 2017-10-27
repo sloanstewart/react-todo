c6b5ea5eb2a57f0e1e5eb7ca99051e6567510f33
Sloan <sloanstewartdev@gmail.com>
2017-10-26 20:25:44 -0400

    conan inspired todo items

--- src/app/index.js
+++ src/app/index.js
@@ -4,11 +4,23 @@ var createReactClass = require('create-react-class');
 
 // Create component
 var TodoComponent = createReactClass({
+    getInitialState: function(){
+        return {
+            todos: ['crush your enemies', 'see them driven before you', 'hear the lamentations of their women']
+        }
+    },
     render: function(){
         return(
-           <h1>yep!</h1>
+            <div id="todo-list">
+                <p>It's a list...of todos!</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
+            </div>
         );
-    }
+    }// render
 });
 
 // Put component into HTML page
