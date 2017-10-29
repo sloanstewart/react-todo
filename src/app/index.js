63340fb441a82e2d8a984ad59b31cb5297521654
Sloan <sloanstewartdev@gmail.com>
2017-10-28 21:15:03 -0400

    refactored to es6, added delete item function

--- src/app/index.js
+++ src/app/index.js
@@ -3,43 +3,63 @@ var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 
 // Create component
-var TodoComponent = createReactClass({
-    getInitialState: function(){
-        return {
-            todos: ['crush your enemies', 'see them driven before you', 'hear the lamentations of their women'],
-            age: 30
+class TodoComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: ['make trap beat', 'crush your enemies', 'see them driven before you', 'hear the lamentations of their women']
         }
-    },
-    render: function(){
+        this.onDelete = this.onDelete.bind(this);
+    }
+    render() {
         var todos = this.state.todos;
-        todos = todos.map(function(item, index){
+        todos = todos.map( (item, index) => { // bind 'this' through arrow function
             return(
-                <TodoItem item={item} key={index} />
+                <TodoItem item={item} key={index} onDelete={this.onDelete} />
             );
         });
         return(
             <div id="todo-list">
                 <p>it's a list of todos!</p>
                 <ul>
-                    {todos}
+                  {todos}
                 </ul>
             </div>
         );
-    }// render
-});
+    }
+
+    //Custom functions
+    onDelete(item){
+        var updatedTodos = this.state.todos.filter(function(val, index){
+            return item !== val; 
+        });
+        this.setState({
+            todos: updatedTodos
+        });
+    }
+};
 
 // TodoItem component
-var TodoItem = createReactClass({
-    render: function(){
+class TodoItem extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    render() {
         return(
             <li>
                 <div className="todo-item">
                     <span className="item-name">{this.props.item}</span>
+                    <span className="item-delete" onClick={this.handleDelete}> x </span>
                 </div>
             </li>
         );
     }
-});
+    // Custom functions
+    handleDelete() {
+        this.props.onDelete(this.props.item);
+    }
+};
 
 // Put component into HTML page
 ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
